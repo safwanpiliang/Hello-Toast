fdb68fc50a7d2d07a396fba972011d424e96012b
safwanpiliang <safwan.arfian@gmail.com>
2025-08-20 22:53:24 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.hellotoast
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import com.example.hellotoast.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            txtNumber.text = number.toString()
+
+            btnCount.setOnClickListener {
+                number++
+                txtNumber.text = number.toString()
+            }
+
+            btnToast.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Count $number",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
